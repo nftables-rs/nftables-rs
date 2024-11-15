81cd4f37387519eb7bfba833e9be13ed5ed728f6
Eric Long <i@hack3r.moe>
2024-11-15 15:16:17 +0800

    feat(helper): add async helpers
    Basically integrates functions from the nftables_async crate here, with both
    tokio and async-process support behind feature gates.
    
    Also corrected some of the behaviour differences in nftables_async, e.g.
    get_current_ruleset_raw passes additional arguments (keeping "-j list ruleset"
    intact) in nftables_async, but replaces the default arguments (except -j) here.

--- src/helper.rs
+++ src/helper.rs
@@ -18,6 +18,9 @@ pub const DEFAULT_NFT: Option<&str> = None;
 /// Do not use additional arguments to the `nft` executable.
 pub const DEFAULT_ARGS: &[&str] = &[];
 
+#[cfg(all(feature = "tokio", feature = "async-process"))]
+compile_error!("features `tokio` and `async-process` are mutually exclusive");
+
 /// Error during `nft` execution.
 #[derive(Error, Debug)]
 pub enum NftablesError {
@@ -88,7 +91,10 @@ where
     A: AsRef<OsStr> + ?Sized + 'a,
     I: IntoIterator<Item = &'a A> + 'a,
 {
-    let mut nft_cmd = get_command(program);
+    let program = program
+        .map(AsRef::as_ref)
+        .unwrap_or(NFT_EXECUTABLE.as_ref());
+    let mut nft_cmd = Command::new(program);
     let nft_cmd = nft_cmd.arg("-j");
     let mut args = args.into_iter();
     let nft_cmd = match args.next() {
@@ -98,16 +104,81 @@ where
     let process_result = nft_cmd.output();
     let process_result = process_result.map_err(|e| NftablesError::NftExecution {
         inner: e,
-        program: nft_cmd.get_program().to_os_string(),
+        program: program.into(),
     })?;
 
-    let stdout = read_output(nft_cmd, process_result.stdout)?;
+    let stdout = read_output(program, process_result.stdout)?;
 
     if !process_result.status.success() {
-        let stderr = read_output(nft_cmd, process_result.stderr)?;
+        let stderr = read_output(program, process_result.stderr)?;
 
         return Err(NftablesError::NftFailed {
-            program: nft_cmd.get_program().to_os_string(),
+            program: program.into(),
+            hint: "getting the current ruleset".to_string(),
+            stdout,
+            stderr,
+        });
+    }
+    Ok(stdout)
+}
+
+#[cfg(any(feature = "tokio", feature = "async-process"))]
+pub async fn get_current_ruleset_async() -> Result<Nftables<'static>, NftablesError> {
+    get_current_ruleset_with_args_async(DEFAULT_NFT, DEFAULT_ARGS).await
+}
+
+#[cfg(any(feature = "tokio", feature = "async-process"))]
+pub async fn get_current_ruleset_with_args_async<'a, P, A, I>(
+    program: Option<&P>,
+    args: I,
+) -> Result<Nftables<'static>, NftablesError>
+where
+    P: AsRef<OsStr> + ?Sized,
+    A: AsRef<OsStr> + ?Sized + 'a,
+    I: IntoIterator<Item = &'a A> + 'a,
+{
+    let output = get_current_ruleset_raw_async(program, args).await?;
+    serde_json::from_str(&output).map_err(NftablesError::NftInvalidJson)
+}
+
+#[cfg(any(feature = "tokio", feature = "async-process"))]
+pub async fn get_current_ruleset_raw_async<'a, P, A, I>(
+    program: Option<&P>,
+    args: I,
+) -> Result<String, NftablesError>
+where
+    P: AsRef<OsStr> + ?Sized,
+    A: AsRef<OsStr> + ?Sized + 'a,
+    I: IntoIterator<Item = &'a A> + 'a,
+{
+    #[cfg(feature = "async-process")]
+    use async_process::Command;
+    #[cfg(feature = "tokio")]
+    use tokio::process::Command;
+
+    let program = program
+        .map(AsRef::as_ref)
+        .unwrap_or(NFT_EXECUTABLE.as_ref());
+    let mut nft_cmd = Command::new(program);
+    let nft_cmd = nft_cmd.arg("-j");
+    let mut args = args.into_iter();
+    let nft_cmd = match args.next() {
+        Some(arg) => nft_cmd.arg(arg).args(args),
+        None => nft_cmd.args(["list", "ruleset"]),
+    };
+    let process_result = nft_cmd.output().await;
+    let process_result = process_result.map_err(|e| NftablesError::NftExecution {
+        inner: e,
+        program: program.into(),
+    })?;
+
+    let stdout = read_output(program, process_result.stdout)?;
+
+    if !process_result.status.success() {
+        let stderr = read_output(program, process_result.stderr)?;
+
+        return Err(NftablesError::NftFailed {
+            program: program.into(),
             hint: "getting the current ruleset".to_string(),
             stdout,
             stderr,
@@ -163,7 +234,10 @@ where
     A: AsRef<OsStr> + ?Sized + 'a,
     I: IntoIterator<Item = &'a A> + 'a,
 {
-    let mut nft_cmd = get_command(program);
+    let program = program
+        .map(AsRef::as_ref)
+        .unwrap_or(NFT_EXECUTABLE.as_ref());
+    let mut nft_cmd = Command::new(program);
     let default_args = ["-j", "-f", "-"];
     let process = nft_cmd
         .args(args)
@@ -172,7 +246,7 @@ where
         .stdout(Stdio::piped())
         .spawn();
     let mut process = process.map_err(|e| NftablesError::NftExecution {
-        program: nft_cmd.get_program().to_os_string(),
+        program: program.into(),
         inner: e,
     })?;
 
@@ -180,7 +254,7 @@ where
     stdin
         .write_all(payload.as_bytes())
         .map_err(|e| NftablesError::NftExecution {
-            program: nft_cmd.get_program().to_os_string(),
+            program: program.into(),
             inner: e,
         })?;
     drop(stdin);
@@ -189,33 +263,116 @@ where
     match result {
         Ok(output) if output.status.success() => Ok(()),
         Ok(process_result) => {
-            let stdout = read_output(&nft_cmd, process_result.stdout)?;
-            let stderr = read_output(&nft_cmd, process_result.stderr)?;
+            let stdout = read_output(program, process_result.stdout)?;
+            let stderr = read_output(program, process_result.stderr)?;
 
             Err(NftablesError::NftFailed {
-                program: nft_cmd.get_program().to_os_string(),
+                program: program.into(),
                 hint: "applying ruleset".to_string(),
                 stdout,
                 stderr,
             })
         }
         Err(e) => Err(NftablesError::NftExecution {
-            program: nft_cmd.get_program().to_os_string(),
+            program: program.into(),
             inner: e,
         }),
     }
 }
 
-fn get_command<S: AsRef<OsStr>>(program: Option<S>) -> Command {
-    match program {
-        Some(program) => Command::new(program),
-        None => Command::new(NFT_EXECUTABLE),
+#[cfg(any(feature = "tokio", feature = "async-process"))]
+pub async fn apply_ruleset_async(nftables: &Nftables<'_>) -> Result<(), NftablesError> {
+    apply_ruleset_with_args_async(nftables, DEFAULT_NFT, DEFAULT_ARGS).await
+}
+
+#[cfg(any(feature = "tokio", feature = "async-process"))]
+pub async fn apply_ruleset_with_args_async<'a, P, A, I>(
+    nftables: &Nftables<'_>,
+    program: Option<&P>,
+    args: I,
+) -> Result<(), NftablesError>
+where
+    P: AsRef<OsStr> + ?Sized,
+    A: AsRef<OsStr> + ?Sized + 'a,
+    I: IntoIterator<Item = &'a A> + 'a,
+{
+    let nftables = serde_json::to_string(nftables).expect("failed to serialize Nftables struct");
+    apply_ruleset_raw_async(&nftables, program, args).await
+}
+
+#[cfg(any(feature = "tokio", feature = "async-process"))]
+pub async fn apply_ruleset_raw_async<'a, P, A, I>(
+    payload: &str,
+    program: Option<&P>,
+    args: I,
+) -> Result<(), NftablesError>
+where
+    P: AsRef<OsStr> + ?Sized,
+    A: AsRef<OsStr> + ?Sized + 'a,
+    I: IntoIterator<Item = &'a A> + 'a,
+{
+    #[cfg(feature = "async-process")]
+    use async_process::Command;
+    #[cfg(feature = "async-process")]
+    use futures_lite::io::AsyncWriteExt;
+    #[cfg(feature = "tokio")]
+    use tokio::io::AsyncWriteExt;
+    #[cfg(feature = "tokio")]
+    use tokio::process::Command;
+
+    let program = program
+        .map(AsRef::as_ref)
+        .unwrap_or(NFT_EXECUTABLE.as_ref());
+    let mut nft_cmd = Command::new(program);
+    let default_args = ["-j", "-f", "-"];
+    let process = nft_cmd
+        .args(args)
+        .args(default_args)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn();
+    let mut process = process.map_err(|e| NftablesError::NftExecution {
+        program: program.into(),
+        inner: e,
+    })?;
+
+    let mut stdin = process.stdin.take().unwrap();
+    stdin
+        .write_all(payload.as_bytes())
+        .await
+        .map_err(|e| NftablesError::NftExecution {
+            program: program.into(),
+            inner: e,
+        })?;
+    drop(stdin);
+
+    #[cfg(feature = "tokio")]
+    let result = process.wait_with_output().await;
+    #[cfg(feature = "async-process")]
+    let result = process.output().await;
+    match result {
+        Ok(output) if output.status.success() => Ok(()),
+        Ok(process_result) => {
+            let stdout = read_output(program, process_result.stdout)?;
+            let stderr = read_output(program, process_result.stderr)?;
+
+            Err(NftablesError::NftFailed {
+                program: program.into(),
+                hint: "applying ruleset".to_string(),
+                stdout,
+                stderr,
+            })
+        }
+        Err(e) => Err(NftablesError::NftExecution {
+            program: program.into(),
+            inner: e,
+        }),
     }
 }
 
-fn read_output(cmd: &Command, bytes: Vec<u8>) -> Result<String, NftablesError> {
+fn read_output(program: impl Into<OsString>, bytes: Vec<u8>) -> Result<String, NftablesError> {
     String::from_utf8(bytes).map_err(|e| NftablesError::NftOutputEncoding {
         inner: e,
-        program: cmd.get_program().to_os_string(),
+        program: program.into(),
     })
 }
